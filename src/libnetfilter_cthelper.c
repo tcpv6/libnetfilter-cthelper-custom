663bf4b65b3bfbbbc355c3ae0daa8e5be1873483
Pablo Neira Ayuso <pablo@netfilter.org>
2012-06-05 18:55:17 +0200

    fix licensing terms
    The licensing terms of this library were pretty inconsistent.
    
    This library is GPLv2+.
    
    Thanks to Jan Engelhardt and openSUSE legal team for spotting this.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/libnetfilter_cthelper.c
+++ src/libnetfilter_cthelper.c
@@ -2,8 +2,8 @@
  * (C) 2012 by Pablo Neira Ayuso <pablo@netfilter.org>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the Lesser GNU General Public License as published
- * by the Free Software Foundation; either version 2.1 of the License, or
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * This code has been sponsored by Vyatta Inc. <http://www.vyatta.com>
